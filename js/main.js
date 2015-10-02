74d3e0fed5262a5dce8d7942ca4fcf3dafd043f4
ebolivong <ebolivong@gmail.com>
2015-10-02 14:28:48 -0400

    add and removed classes

--- js/main.js
+++ js/main.js
@@ -1,2 +1,24 @@
+$('button').on('click', function(e){
 
+	var button = $('button').hasClass('on');
+ 
+ 	if ( button == true) {
 
+ 		$('button').removeClass('on');
+
+ 		$('button').addClass('off');
+
+ 		$('body').addClass('dark');
+ 	}
+
+ 	else if ( button == false) {
+
+ 		$('button').removeClass('off');
+ 		$('button').addClass('on');
+ 		$('body').removeClass('dark');
+ 		$('body').addclass('light');
+ 	}
+
+ });
+
+ 
